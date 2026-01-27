c75a132422624ec75b29b05e72f66a9984598541
Adinaxgg-git <adinabektursyn2007@icloud.com>
2026-01-27 21:15:05 +0500

    mypractice1 in progress

---
+++ mypractice1/strings.py
@@ -0,0 +1,21 @@
+#1 Example 1: Print a string
+s = "Hello, Python!"
+print(s)
+
+#2 Example 2: Length of a string
+s = "Programming"
+print(len(s))
+
+#3 Example 3: Upper and lower case
+s = "Hello World"
+print(s.upper())
+print(s.lower())
+
+#4 Example 4: String concatenation
+name = "Adina"
+surname = "Bektursyn"
+print(name + " " + surname)
+
+#5 Example 5: Check substring
+s = "I love Python"
+print("Python" in s)
